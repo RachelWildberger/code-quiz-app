e3b64073a3905c0dd67f4e0b540d2974c7d75bec
Rachel L Wildberger <rachellwildberger@Rachels-MacBook-Pro.local>
2022-12-04 15:31:17 -1000

    made edits to project CSS, HTML, JS files. Added questions.js file.

---
+++ assets/js/questions.js
@@ -0,0 +1,41 @@
+var questions = [
+    {
+        question: "What are the four things required to call a method?",
+        option1: "A data type, a comma, a method name, and braces.", 
+        option2: "A Number, Boolean, Null, and String value.", 
+        option3: "An instance of data, a period, a method name, and parentheses.", 
+        option4: "You can not call a method.",
+
+        correctAnswer: "An instance of data, a period, a method name, and parentheses.",
+    },
+
+    {
+        question: "In the following code what will x evaluate to? let x = 14 % 2",
+        option1: "x = 0", 
+        option2: "x = 2", 
+        option3: "x = 7",
+        option4: "x = 28",
+
+        correctAnswer: "x = 0",
+    },
+
+    {
+        question: "What is the correct syntax for an array?",
+        option1: "var myArray = 'Los Angeles', 'New York', 'Chicago', Houston';", 
+        option2: "var myArray = {'Los Angeles', 'New York', 'Chicago', Houston'};", 
+        option3: "var myArray = ['Los Angeles', 'New York', 'Chicago' Houston'];", 
+        option4: "var myArray = ['Los Angeles'; 'New York'; 'Chicago'; Houston';",
+
+        correctAnswer: "var myArray = ['Los Angeles', 'New York', 'Chicago' Houston'];",
+    },
+
+    {
+        question: "Which of the following methods is used to access HTML elements using JavaScript?",
+        option1: "getElementById()", 
+        option2: "getElementByClassName()", 
+        option3: "getElementByTagName();", 
+        option4: "All of the above",
+
+        correctAnswer: "All of the above",
+    },
+]
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,11 @@
 var start = document.querySelector("#start");
+var container = document.querySelector(".container");
 
 var startQuizButton = document.querySelector("#start-quiz");
 var highScoresButton = document.querySelector("#highscores");
 
 
-var question = document.querySelector("#question");
+var question = document.querySelector("question");
 var questionNo = document.querySelector("#questionNo");
 var questionText = document.querySelector("#questionText");
 
@@ -13,76 +14,109 @@ var choiceOptions = document.querySelectorAll(".choiceOptions");
 var questionContainer = document.querySelector("#questionContainer");
 
 var answers = document.querySelector("#answersSection");
+var time = document.querySelector("#time");
 
 var option1 = document.querySelector("#option1");
 var option2 = document.querySelector("#option2");
 var option3 = document.querySelector("#option3");
 var option4 = document.querySelector("#option4");
 
-var totalCorrect = document.querySelector("#total-correct");
 var nextQuestion = document.querySelector("#next-question");
 
 var questionInProgress = false;
-
+var checkHighScore = false;
 
 var currentQuestion = 0;
 var score = 0;
 var index = 0;
 var correct = 0;
+var timer = 100;
+
 
 // when the start button clicks
-startQuizButton.addEventListener("click", function() {
-console.log('click');
-   if (questionInProgress === false) {
-    questionInProgress = true;
-    questionContainer.setAttribute("class", "visible");
-  }
-});
-
-startQuizButton.addEventListener("click", function(){
-    start.style.display = "none";
-});
-
-// loop of questions
- for(let i = 0; i < questions.length; i++) {
-    console.log(questions[i]);
-}
-
-choiceOptions.forEach((choices, choiceOptions) => {
-    choices.addEventListener("click", () => {
-        choices.classList.add("active");
-        //check answer
-        if (choiceOptions === questions[index].answer) {
-            correct++;
-        } else {
-            correct += 0;
-        }
-
-        // //disable All Options When User Select An Option
-        // for (i = 0; i <= 3; i++) {
-        //     choiceOptions[i].classList.add("disabled");
-        // }
-    })
-});    
-
-// startgame
- startGame = () => {
-    availableQuestions = [...questions]
-    getNewQuestion()
- }    
- 
-
-//what happens when the next button is clicked
-nextQuestion.addEventListener("click", function() {
+    startQuizButton.addEventListener("click", function() {
     console.log('click');
-       if (questionInProgress === false) {
+    if (questionInProgress === false) {
         questionInProgress = true;
         questionContainer.setAttribute("class", "visible");
-      }
+    }
     });
 
+    startQuizButton.addEventListener("click", function(){
+        start.style.display = "none";
+    });
+    var buttons = document.querySelectorAll("#optionList")
 
-//   else {
-//     mode = "dark";
-//     container.setAttribute("class", "dark");
-//   }
+// load questions 
+    var loadQuestions = function() {
+        questionNo.innerText = index + 1 + ".";
+        questionText.innerText = questions[index].question;
+        option1.innerText = questions[index].option1;
+        option2.innerText = questions[index].option2;
+        option3.innerText = questions[index].option3;
+        option4.innerText = questions[index].option4;
+     }
+
+
+
+// start quiz starts timer
+    startQuizButton.addEventListener("click", function(){
+        start.style.display = "none";
+
+        interval = setInterval(countDown, 1000);
+        loadQuestions();
+    });
+
+  
+
+// Timer
+    var countDown = function() {
+        if(timer <= 0)
+        {
+        clearInterval(interval);
+        } else {
+            timer--;
+            console.log(timer);
+            time.textContent = timer;
+        }
+     }
+
+
+
+// answers section
+
+var correctAnswer = true;
+
+        function answerClick(event) {
+            var choice = event.target;
+            console.log(choice.textContent);
+            if (choice.textContent == questions[index].correctAnswer){
+                // code for correct 
+             } else {
+                timer -= 10;
+             }
+             nextSetOfQuestions();
+        };
+
+        function nextSetOfQuestions() {
+            console.log('click');
+            if (index !== questions.length - 1) {
+                index++;
+                loadQuestions();
+            }
+            }
+
+    option1.addEventListener("click", answerClick);
+    option2.addEventListener("click", answerClick);
+    option3.addEventListener("click", answerClick);
+    option4.addEventListener("click", answerClick);
+
+
+// high Scores button
+    highScoresButton.addEventListener("click", function() {
+        console.log('click');
+        if (checkHighScore === false) {
+            checkHighScore = true;
+            container.setAttribute("class", "hidden");
+        }
+        });
